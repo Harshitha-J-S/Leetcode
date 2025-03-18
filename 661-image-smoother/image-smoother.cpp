e444ee866d9f275b28adf3d01b962bca0ba519ca
HarshithaJS <jsharshithagowda@gmail.com>
2025-03-18 06:07:39 +0530

    Update image-smoother.cpp

--- 661-image-smoother/image-smoother.cpp
+++ 661-image-smoother/image-smoother.cpp
@@ -6,7 +6,7 @@ public:
         vector<vector<int>> result(m, vector<int>(n, 0));
 
          vector<pair<int, int>> directions = {
-        {-1, -1}, {-1, 0}, {-1, 1},
+        {-1, -1}, {-1, 0}, {-1, 1},   
         { 0, -1}, { 0, 0}, { 0, 1},
         { 1, -1}, { 1, 0}, { 1, 1}
     };
@@ -17,11 +17,11 @@ public:
 
             // Check all 9 directions
             for (auto [dx, dy] : directions) {
-                int x = i + dx;
+                int x = i + dx;          // to move in all directions
                 int y = j + dy;
 
                 // Check if neighbor is within bounds
-                if (x >= 0 && x < m && y >= 0 && y < n) {
+                if (x >= 0 && x < m && y >= 0 && y < n) {  // ensure tat x and y are valid
                     totalSum += img[x][y];
                     count++;
                 }
@@ -35,5 +35,13 @@ public:
     return result;
     }
 };
-
+/* {-1, -1} → Move 1 step up & left (top-left).
+{-1, 0} → Move 1 step up (top).
+{-1, 1} → Move 1 step up & right (top-right).
+{0, -1} → Move 1 step left.
+{0, 0} → Current pixel.
+{0, 1} → Move 1 step right.
+{1, -1} → Move 1 step down & left (bottom-left).
+{1, 0} → Move 1 step down (bottom).
+{1, 1} → Move 1 step down & right (bottom-right).*/
  
